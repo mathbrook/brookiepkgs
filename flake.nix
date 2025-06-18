0d6d35a196be1cf5faf2e0bec7dcc744152dd582
Matthew Samson <matthew.samson@oxos.com>
2025-06-18 11:23:48 -0400

    yeet

--- flake.nix
+++ flake.nix
@@ -3,9 +3,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     devshell.url = "github:numtide/devshell";
     nix-proto.url = "github:notalltim/nix-proto";
+    nix-proto.inputs.nixpkgs.follows = "nixpkgs";
     nebs-packages.url = "github:RCMast3r/nebs_packages";
+    nebs-packages.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, flake-parts, devshell, nebs-packages, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; }
@@ -20,17 +23,20 @@
         ];
         perSystem = { config, pkgs, system, ... }:
           let
+            nebs_overlays = [
+              inputs.nebs-packages.overlays.default
+            ];
             foxglove-studio = pkgs.callPackage ./foxglove-studio.nix { inherit system; };
           in
           {
             packages.foxglove-studio = foxglove-studio;
             overlayAttrs = {
-              inherit (config.packages) default foxglove-studio;
+              inherit (config.packages) default foxglove-studio nebs_overlays;
             };
             legacyPackages =
               import nixpkgs {
                 inherit system;
-                overlays = [ self.overlays.default ];
+                overlays = [ self.overlays.default nebs_overlays ];
               };
           };
       };
